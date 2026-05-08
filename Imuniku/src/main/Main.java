2664848dc77816f142b5697fd9ea579d4eb8f7d1
Choqy Pananda Sirait <krisky325@gmail.com>
2026-05-08 10:54:58 +0700

    Create Main.java

---
+++ Imuniku/src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("       IMUNIKU SYSTEM");
+        System.out.println(" Sistem Informasi Imunisasi");
+        System.out.println("=================================");
+
+        System.out.println("Project Java berhasil dijalankan!");
+    }
+}
